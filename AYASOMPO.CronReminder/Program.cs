6cc7b2f38453d2029114e7ddb0dfa5533e041917
SuThetKhaing <suthetkhaing362@gmail.com>
2024-05-09 16:23:56 +0630

    To wake up multiple urls

--- AYASOMPO.CronReminder/Program.cs
+++ AYASOMPO.CronReminder/Program.cs
@@ -1,22 +1,36 @@
 ﻿using AYASOMPO.CronReminder.Services;
 
-string? url;
+List<string>? urls = new List<string>();
 string? minute;
 
-Console.WriteLine("Enter url to be awaken");
-url = Console.ReadLine();
+Console.WriteLine("Enter the number of URLs to be awakened:");
+if (!int.TryParse(Console.ReadLine(), out int count) || count <= 0)
+{
+    throw new ArgumentException("Invalid number of URLs specified.");
+}
+
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine($"Enter URL {i + 1}:");
+    string url = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(url)) throw new Exception("Url must be specified.");
+
+    urls.Add(url); 
+}
 
 Console.WriteLine("Enter minute to awake url");
 minute = Console.ReadLine();
 
-if (string.IsNullOrWhiteSpace(url)) throw new Exception("Url must be specified.");
 if (string.IsNullOrWhiteSpace(minute)) throw new Exception("Minutes must be specified.");
 
 Console.WriteLine("Timer Start...\n");
 Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
 
 TimerService _service = new TimerService();
-_service.SetTimer(url, Convert.ToInt32(minute));
+_service.SetTimer(urls, Convert.ToInt32(minute));
 
 Console.WriteLine("\nPress the Enter key to exit the application...\n");
 Console.ReadLine();
+
+_service.Dispose();
--- AYASOMPO.CronReminder/Services/TimerService.cs
+++ AYASOMPO.CronReminder/Services/TimerService.cs
@@ -1,4 +1,5 @@
 ﻿using RestSharp;
+using System.Threading;
 using System.Timers;
 
 namespace AYASOMPO.CronReminder.Services
@@ -6,35 +7,33 @@ namespace AYASOMPO.CronReminder.Services
     public class TimerService
     {
         private System.Timers.Timer aTimer;
-        private string? _url;
+        private List<string>? _urls;
 
-        public void SetTimer(string url, int time)
+        public void SetTimer(List<string> urls, int time)
         {
-            _url = url;
+            _urls = urls;
 
             time = (int)TimeSpan.FromMinutes(time).TotalMilliseconds;
             // Create a timer with a two second interval.
             aTimer = new System.Timers.Timer(time);
             // Hook up the Elapsed event for the timer. 
-            aTimer.Elapsed += OnTimedEvent;
-            aTimer.AutoReset = true;
-            aTimer.Enabled = true;
+            //aTimer.Elapsed += OnTimedEvent;
+            //aTimer.AutoReset = true;
+            //aTimer.Enabled = true;
+
+            for (int i = 0; i < urls.Count; i++)
+            {
+                aTimer.Elapsed += (sender, e) => OnTimedEvent(urls[i], e);
+                aTimer.AutoReset = true;
+                aTimer.Enabled = true;
+                //aTimer.Add(timer);
+            }
         }
 
-        private async void OnTimedEvent(Object source, ElapsedEventArgs e)
+        private async void OnTimedEvent(string url, ElapsedEventArgs e)
         {
-            //using (var client = new HttpClient())
-            //{
-            //    client.BaseAddress = new Uri("https://localhost:7169");
-            //    var response = await client.GetAsync("");
-            //    Console.WriteLine("\nCall Success...\n");
-            //}
-            //string endpoint = "https://localhost:7169";
-            //string endpoint = "http://localhost:8024";
-
-            string endpoint = _url;
             RestClient client = new RestClient();
-            RestRequest request = new RestRequest(endpoint, Method.Get);
+            RestRequest request = new RestRequest(url, Method.Get);
             var response = await client.GetAsync(request);
 
             if (response.IsSuccessful)
@@ -42,7 +41,38 @@ namespace AYASOMPO.CronReminder.Services
             else
                 Console.WriteLine($"Error: {response.StatusCode}");
 
-            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",                              e.SignalTime);
+            Console.WriteLine($"The Elapsed event for {url} was raised at {e.SignalTime:HH:mm:ss.fff}");
         }
+
+        public void Dispose()
+        {
+            aTimer.Dispose();
+        }
+
+        #region for One Url(Not Used)
+        //private async void OnTimedEvent(Object source, ElapsedEventArgs e)
+        //{
+        //    //using (var client = new HttpClient())
+        //    //{
+        //    //    client.BaseAddress = new Uri("https://localhost:7169");
+        //    //    var response = await client.GetAsync("");
+        //    //    Console.WriteLine("\nCall Success...\n");
+        //    //}
+        //    //string endpoint = "https://localhost:7169";
+        //    //string endpoint = "http://localhost:8024";
+
+        //    string endpoint = _url;
+        //    RestClient client = new RestClient();
+        //    RestRequest request = new RestRequest(endpoint, Method.Get);
+        //    var response = await client.GetAsync(request);
+
+        //    if (response.IsSuccessful)
+        //        Console.WriteLine(response.StatusCode);
+        //    else
+        //        Console.WriteLine($"Error: {response.StatusCode}");
+
+        //    Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",                              e.SignalTime);
+        //}
+        #endregion
     }
 }
