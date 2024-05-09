1f4a63f1ffb17be16756fcf3289841e50e47656a
SuThetKhaing <suthetkhaing362@gmail.com>
2024-05-09 15:34:27 +0630

    Add project files.

---
+++ AYASOMPO.CronReminder/Program.cs
@@ -0,0 +1,22 @@
+﻿using AYASOMPO.CronReminder.Services;
+
+string? url;
+string? minute;
+
+Console.WriteLine("Enter url to be awaken");
+url = Console.ReadLine();
+
+Console.WriteLine("Enter minute to awake url");
+minute = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(url)) throw new Exception("Url must be specified.");
+if (string.IsNullOrWhiteSpace(minute)) throw new Exception("Minutes must be specified.");
+
+Console.WriteLine("Timer Start...\n");
+Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
+
+TimerService _service = new TimerService();
+_service.SetTimer(url, Convert.ToInt32(minute));
+
+Console.WriteLine("\nPress the Enter key to exit the application...\n");
+Console.ReadLine();
---
+++ AYASOMPO.CronReminder/Services/TimerService.cs
@@ -0,0 +1,48 @@
+﻿using RestSharp;
+using System.Timers;
+
+namespace AYASOMPO.CronReminder.Services
+{
+    public class TimerService
+    {
+        private System.Timers.Timer aTimer;
+        private string? _url;
+
+        public void SetTimer(string url, int time)
+        {
+            _url = url;
+
+            time = (int)TimeSpan.FromMinutes(time).TotalMilliseconds;
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(time);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        private async void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            //using (var client = new HttpClient())
+            //{
+            //    client.BaseAddress = new Uri("https://localhost:7169");
+            //    var response = await client.GetAsync("");
+            //    Console.WriteLine("\nCall Success...\n");
+            //}
+            //string endpoint = "https://localhost:7169";
+            //string endpoint = "http://localhost:8024";
+
+            string endpoint = _url;
+            RestClient client = new RestClient();
+            RestRequest request = new RestRequest(endpoint, Method.Get);
+            var response = await client.GetAsync(request);
+
+            if (response.IsSuccessful)
+                Console.WriteLine(response.StatusCode);
+            else
+                Console.WriteLine($"Error: {response.StatusCode}");
+
+            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",                              e.SignalTime);
+        }
+    }
+}
